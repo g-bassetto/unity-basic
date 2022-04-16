ae6036360feaf20f5fe0abf10d526afd271bcb85
Giacomo Bassetto <giacomo.bassetto@gmail.com>
2022-04-16 09:56:46 +0200

    Added animation and reverted to default rendering pipeline.

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -6,18 +6,29 @@ public class Graph : MonoBehaviour {
 
     [SerializeField, Range(10, 100)]
     int resolution = 10;
+    Transform[] points;
 
     void Awake() {
         float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-        for (int i = 0;  i < resolution; i++) {
-            Transform point = Instantiate(pointPrefab);
+        points = new Transform[resolution];
+        for (int i = 0;  i < points.Length; i++) {
+            var point = points[i] = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
+        }
+    }
+
+    private void Update() {
+        var time = Time.time;
+        for (int i = 0; i < points.Length; i++) {
+            var point = points[i];
+            var pos = point.localPosition;
+            pos.y = Mathf.Sin(Mathf.PI * (pos.x + time));
+            point.localPosition = pos;
         }
     }
 }
